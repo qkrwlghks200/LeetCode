9ddcbbbec5c5d323071705e8a1a132e1ce5e44b3
JiHwan <qkrwlghks200@gmail.com>
2022-07-19 20:06:55 +0900

    web done

--- src/main/java/com/codepresso/todo/controller/TodoController.java
+++ src/main/java/com/codepresso/todo/controller/TodoController.java
@@ -1,34 +1,47 @@
 package com.codepresso.todo.controller;
 
-import java.util.List;
-
+import com.codepresso.todo.service.TodoService;
 import com.codepresso.todo.vo.ResultDto;
 import com.codepresso.todo.vo.Todo;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/todo")
 public class TodoController {
 
     //todo TodoService를 활용하기 위해 의존성 주입 코드를 작성하세요
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
 
     //todo API문서를 확인하여 GET /todo REST API를 완성하세요
     //todo TodoService를 활용하세요
+    @GetMapping
     public List<Todo> getTodoList(){
-        List<Todo> list=null;
+        List<Todo> list = todoService.getTodoList();
         return list;
     }
 
     //todo API문서를 확인하여 POST /todo REST API를 완성하세요
     //todo TodoService를 활용하세요
-    public ResultDto addTodo(Todo todo) {
-
+    @PostMapping
+    public ResultDto addTodo(@RequestBody Todo todo) {
+        Todo todoparam = new Todo(todo.getContent());
+        todoService.addTodo(todoparam);
         //todo return code는 변경하지 마세요
         return new ResultDto(200, "Success");
     }
 
     //todo API문서를 확인하여 DELETE /todo REST API를 완성하세요
     //todo TodoService를 활용하세요
-    public ResultDto deleteTodo(@PathVariable("id") Integer id) {
-
+    @DeleteMapping(value = "/{id}")
+    public ResultDto deleteTodo(@PathVariable(name = "id", required = true) String id) {
+        int idx = Integer.parseInt(id);
+        todoService.deleteTodo(idx);
         //todo return code는 변경하지 마세요
         return new ResultDto(200, "Success");
     }
--- src/main/java/com/codepresso/todo/service/TodoService.java
+++ src/main/java/com/codepresso/todo/service/TodoService.java
@@ -3,23 +3,32 @@ package com.codepresso.todo.service;
 import java.util.List;
 
 import com.codepresso.todo.vo.Todo;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Service
 public class TodoService {
 
     private List<Todo> todoList;
 
     //todo 멤버 변수 todoList에 의존성 주입을 하기 위한 코드를 완성하세요
+    public TodoService(List<Todo> todoList) {
+        this.todoList = todoList;
+    }
 
     public void addTodo(Todo todo) {
         //todo todoList에 새로운 항목을 추가하세요
+        todo.setId(todoList.size());
+        todoList.add(todo);
     }
 
     public List<Todo> getTodoList(){
         //todo todoList 정보들을 반환 하세요
-        return null;
+        return todoList;
     }
 
     public void deleteTodo(int index) {
         //todo todoList에서 index에 해당하는 항목을 삭제하세요
+        todoList.remove(index);
     }
 }
